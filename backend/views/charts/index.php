1e7571b87325c0de7a40482ee486b50bb0fdf4ad
danielraimondi <daniel.raimondi@gmail.com>
2015-11-09 15:18:30 +0000

    usuarios listados en Select

--- backend/views/charts/index.php
+++ backend/views/charts/index.php
@@ -1,50 +1,12 @@
 
 <?php
-
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
 use yii\widgets\ActiveForm;
 use backend\models\chart_Survey;
-
 $this->title = Yii::t('app', 'Charts');
 $this->params['breadcrumbs'][] = $this->title;
-
-
-
-
-
-
-//los 3 productos mas comprados por ese cliente
-//$client_opt_order = Yii::app()->db->createCommand()->
-//                      select('client_id, prod_id, order')->
-//                      from('survey')->
-//                      where('client_id=1')->
-//                      order('order DESC')->
-//                      limit(3)->
-//                      queryAll();          
-                      
-                      
- //los clientes que comprarn
- 
- //$client_survey->select('client_id')->
- //                     from('survey')->
- //                     order('order DESC')->
- //                     queryAll();             
- //                     
-//$client_survey = Yii::app()->db->createCommand()->
-
-
-//$client_survey = Yii::t('app', 'survey')->db->createCommand()
-//                    ->select('client_id,prod_id,order')
-//                     ->from('survey')
-//                    ->where('client_id=:1')
-//                   ->group('order')
-//                   ->query();
-        
-        
-//$cID = Yii::app()->survey->client_id;
-
 ?>
 
 
@@ -57,64 +19,44 @@ $this->params['breadcrumbs'][] = $this->title;
      
      <?php 
      
-      $id = 1;
-      //---op.2---------
-  /*   $client_id = Yii::$app->db->createCommand(
-                      'SELECT  `client_id` ,  `prod_id` ,  `order` 
-                      FROM  `survey`
-                      WHERE client_id=1
-                      ORDER BY  `order` DESC 
-                      LIMIT 3'
-                      )->queryAll();
-    */                  
-//    
-//     print_r( $client_id[0]);
-//     echo "----";
-//     print_r( $client_id[1]);
- //    echo "----";
- //    print_r( $client_id[2]);
-//     $id = '1';
+     //Trae TODOS los clientes que realizaron alguna compra
+    $total_cli = (new \yii\db\Query())
+    ->select('client_id')
+    ->distinct()
+    ->from('survey');
+     
+    $command_total_cli = $total_cli->createCommand();
+    $data = $command_total_cli->queryAll();
+    $i = 0;
+    //-----------------------------------------------------
 
 
+     $client = 1;
+     //Trae las 3 COMPRAS más altas de un cliente  $client
     $query = (new \yii\db\Query())
     ->select('client_id, prod_id, order')
     ->from('survey')
+    ->where('client_id=:id', array(':id'=>$client))
     ->orderBy(['order' => SORT_DESC,])
     ->limit(3);
     // Crea un commando
       $command = $query->createCommand();
       // Ejecuta el commando
       $rows = $command->queryAll(); //TRAE un array con los datos
-      /*
-      print_r( $rows);
-       echo $rows[0]['prod_id'];
-       echo $rows[1]['prod_id'];
-       echo $rows[2]['prod_id'];
-      */
-      // Imprime resultados
-      
-      /*---op.1
-     foreach ($query->each() as $usuario) {
-     $usuario representa una fila de datos de la tabla usuario
-     print_r( $usuario);
-     }*/
-     
-     //---op.2
-     //      print_r( $rows[0]);
-     
+
+     //-----------------------------------------------------
      
     ?>
     
       
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
         <script type="text/javascript">
-
       google.load("visualization", "1.1", {packages:["bar"]});
       google.setOnLoadCallback(drawChart);
       function drawChart() {
         
         var data = google.visualization.arrayToDataTable([
-          ['ID productos', 'Sales'],
+          ['ID productos', 'Compras'],
           ['<?php echo $rows[0]['prod_id']?>', <?php echo $rows[0]['order']?>],
           ['<?php echo $rows[1]['prod_id']?>', <?php echo $rows[1]['order']?>],
           ['<?php echo $rows[2]['prod_id']?>', <?php echo $rows[2]['order']?>],
@@ -124,16 +66,13 @@ $this->params['breadcrumbs'][] = $this->title;
           
           
         ]);
-
         var options = {
           chart: {
             title: ' Client with ID <?php echo $rows[0]['client_id']?>',
             subtitle: 'TOP 3 compras',
           }
         };
-
         var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
-
         chart.draw(data, options);
       }
      
@@ -144,26 +83,24 @@ $this->params['breadcrumbs'][] = $this->title;
   </head>
 
   <body>
+    
+    <!--CAJA con los id de los Clientes que compraron-->
+    <div>
+            <i><b>Por favor, selecciona un cliente... </b></i>
+           <!-- <select onChange="mostrarResultados(this.value);" >-->
+            <select >
+                <?php
+                    foreach($data as $key) {
+                        echo '<option value="'.$data[$i]['client_id'].'" selected>'.$data[$i]['client_id'].'</option>';
+                        $i++; 
+                    }
+                    
+                ?>
+            </select>
+        </div>
+
 
     <!--Div that will hold the bar chart-->
     <div id="columnchart_material" style="width: 450px; height: 250px;"></div>
   </body>
 </html>
-
-<!--
- <script>
-            
-                 
-
-                            var valores = eval('.$rows[0].');
-
-                            var e   = valores[0];
-                            var f   = valores[1];
-                            var m   = valores[2];
-                           
-                    
-                    
-                
-    </script>
-    
-    -->
