7bc2ddd5165dd9cd79197321145c449846bd0834
danielraimondi <daniel.raimondi@gmail.com>
2015-11-10 18:51:33 +0000

    login desde goolge+

---
+++ common/config/main-local.php
@@ -0,0 +1,39 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=relevando',
+            'username' => 'danielraimondi',
+            'password' => '',
+            'charset' => 'utf8'
+            ],
+            
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'relevando.com@gmail.com',
+                'password' => 'relevando.1',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+        ],
+        
+        'authClientCollection' => [
+            'class'   => \yii\authclient\Collection::className(),
+            'clients' => [
+                // here is the list of clients you want to use
+                // you can read more in the "Available clients" section
+                'google' => [
+                    'class'        => 'dektrium\user\clients\Google',
+                    'clientId'     => '231187283207-nl6phcbmstc8r8httqerev2sneaqnj78.apps.googleusercontent.com',
+                    'clientSecret' => '4TBuRLRDGIO1IdlGA7pjs2Mt',
+                ],
+            ],
+        ],
+        
+        
+    ]
+];
